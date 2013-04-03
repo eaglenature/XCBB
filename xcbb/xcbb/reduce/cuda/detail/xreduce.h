c120dc111dbb123f64ad61622410706a49584ab7
eaglenature <eaglenature@gmail.com>
2013-04-03 22:18:09 +0200

    Reduce init

---
+++ xcbb/xcbb/reduce/cuda/detail/xreduce.h
@@ -0,0 +1,13 @@
+/*
+ * <xcbb/scan/cuda/detail/xreduce.h>
+ *
+ *  Created on: Apr 3, 2013
+ *      Author: eaglenature@gmail.com
+ */
+
+#ifndef REDUCE_CUDA_DETAIL_XREDUCE_H_
+#define REDUCE_CUDA_DETAIL_XREDUCE_H_
+
+
+
+#endif /* REDUCE_CUDA_DETAIL_XREDUCE_H_ */
---
+++ xcbb/xcbb/reduce/xreduce.h
@@ -0,0 +1,13 @@
+/*
+ * <xcbb/scan/xreduce.h>
+ *
+ *  Created on: Apr 3, 2013
+ *      Author: eaglenature@gmail.com
+ */
+
+#ifndef XREDUCE_H_
+#define XREDUCE_H_
+
+
+
+#endif /* XREDUCE_H_ */
